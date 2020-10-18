e088d4fdb0ce57e7acf79310a5d30ce78fec516b
cdw025 <cdwhiteiv@gmail.com>
2020-10-18 13:34:20 -0500

    Update index.js
    commented out all references to assets

--- routes/index.js
+++ routes/index.js
@@ -26,19 +26,19 @@ router.get('/logout', function(req, res, next) {
   res.redirect('/login');
 });
 
-router.get('/barges', authMiddleware.ensureLoggedIn, function(req, res) {
-  Barge.getBarges().then(barges => {
-    barges = JSON.parse(JSON.stringify(barges));
-    res.render('barges', { title: 'Express', barges: barges });
-  });
-});
+// router.get('/barges', authMiddleware.ensureLoggedIn, function(req, res) {
+//   Barge.getBarges().then(barges => {
+//     barges = JSON.parse(JSON.stringify(barges));
+//     res.render('barges', { title: 'Express', barges: barges });
+//   });
+// });
 
-router.get('/boats', authMiddleware.ensureLoggedIn, function(req, res) {
-  Boat.getBoats().then(boats => {
-    boats = JSON.parse(JSON.stringify(boats));
-    res.render('boats', { title: 'Express', boats: boats });
-  });
-});
+// router.get('/boats', authMiddleware.ensureLoggedIn, function(req, res) {
+//   Boat.getBoats().then(boats => {
+//     boats = JSON.parse(JSON.stringify(boats));
+//     res.render('boats', { title: 'Express', boats: boats });
+//   });
+// });
 
 // router.get('/dashboard', authMiddleware.ensureLoggedIn, function(req, res) {
 //   Job.getJobs().then(jobs => {
@@ -48,11 +48,11 @@ router.get('/boats', authMiddleware.ensureLoggedIn, function(req, res) {
 // });
 
 router.get('/dashboard', authMiddleware.ensureLoggedIn, function(req, res) {
-  Asset.getAssets().then(assets => {
+  // Asset.getAssets().then(assets => {
     Job.getJobs().then(jobs => {
       // AssetLog.getLogs().then(logs => {
     jobs = JSON.parse(JSON.stringify(jobs));
-    assets = JSON.parse(JSON.stringify(assets));
+    // assets = JSON.parse(JSON.stringify(assets));
     // logs = JSON.parse(JSON.stringify(logs));
     jobs = jobs.sort((a, b) => parseFloat(a.order_id) - parseFloat(b.order_id));
     // logs = logs.sort((a, b) => parseFloat(b.log_id) - parseFloat(a.log_id));
@@ -78,41 +78,41 @@ router.get('/dashboard', authMiddleware.ensureLoggedIn, function(req, res) {
   });
   // });
   });
-    });
+    // });
 });
 
 
-router.get('/assets', authMiddleware.ensureLoggedIn, function(req, res) {
-  Asset.getAssets().then(assets => {
-    Job.getJobs().then(jobs => {
-      AssetLog.getLogs().then(logs => {
-    jobs = JSON.parse(JSON.stringify(jobs));
-    assets = JSON.parse(JSON.stringify(assets));
-    logs = JSON.parse(JSON.stringify(logs));
-    jobs = jobs.sort((a, b) => parseFloat(a.order_id) - parseFloat(b.order_id));
-    logs = logs.sort((a, b) => parseFloat(b.log_id) - parseFloat(a.log_id));
-            // javascript function to find max log_id value for each asset_name 
-            // in order to only print one result per asset name
-            const arrayFiltered = [];
-            logs.forEach(obj => {
-                const item = arrayFiltered.find(thisItem => thisItem.asset_name === obj.asset_name);
-                if (item) {
-                    if (item.log_id < obj.log_id) {
-                        item.log_id = obj.log_id;
-                    }
-                    return;
-                }
-                arrayFiltered.push(obj);
-            });
+// router.get('/assets', authMiddleware.ensureLoggedIn, function(req, res) {
+//   Asset.getAssets().then(assets => {
+//     Job.getJobs().then(jobs => {
+//       AssetLog.getLogs().then(logs => {
+//     jobs = JSON.parse(JSON.stringify(jobs));
+//     assets = JSON.parse(JSON.stringify(assets));
+//     logs = JSON.parse(JSON.stringify(logs));
+//     jobs = jobs.sort((a, b) => parseFloat(a.order_id) - parseFloat(b.order_id));
+//     logs = logs.sort((a, b) => parseFloat(b.log_id) - parseFloat(a.log_id));
+//             // javascript function to find max log_id value for each asset_name 
+//             // in order to only print one result per asset name
+//             const arrayFiltered = [];
+//             logs.forEach(obj => {
+//                 const item = arrayFiltered.find(thisItem => thisItem.asset_name === obj.asset_name);
+//                 if (item) {
+//                     if (item.log_id < obj.log_id) {
+//                         item.log_id = obj.log_id;
+//                     }
+//                     return;
+//                 }
+//                 arrayFiltered.push(obj);
+//             });
 
 
 
-    console.log(arrayFiltered);
-    res.render('assets', { title: 'Express', jobs: jobs, assets: assets, logs: arrayFiltered });
-  });
-  });
-    });
-});
+//     console.log(arrayFiltered);
+//     res.render('assets', { title: 'Express', jobs: jobs, assets: assets, logs: arrayFiltered });
+//   });
+//   });
+//     });
+// });
 
 
 module.exports = router;
