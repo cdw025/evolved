8d8c4f0886ada17991222ead439787bad3933efc
cdw025 <cdwhiteiv@gmail.com>
2021-03-08 11:54:45 -0600

    trying to fix!
    added PGSSLMODE=no=verify on postgres in addition to code changes

--- knexfile.js
+++ knexfile.js
@@ -9,8 +9,11 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL
+    connection: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
   }
+}
 }; 
  
  
