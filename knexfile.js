ace90126ac908971d3157e68c7c7357e23b31455
cdw025 <cdwhiteiv@gmail.com>
2021-03-08 10:58:49 -0600

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -10,12 +10,10 @@ module.exports = {
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
-    ssl: true,
-    extra: {
-      ssl: {
-        rejectUnauthorized: false
-      },
-    },
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
   }
 }; 
  
