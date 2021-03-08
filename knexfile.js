7de5a26cf013bb28ddfd86da2c332fd979021daf
cdw025 <cdwhiteiv@gmail.com>
2021-03-08 09:59:45 -0600

    fixing again for ssl
    added ssl: { rejectUnauthorized: false }

--- knexfile.js
+++ knexfile.js
@@ -9,7 +9,8 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL + '?ssl=true'
+    connection: process.env.DATABASE_URL + '?ssl=true',
+    ssl: { rejectUnauthorized: false }
   }
 }; 
  
